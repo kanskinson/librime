3f29fa1bba728a205ad4998b71deaeb24bbef0cd
kanskinson <39592070+kanskinson@users.noreply.github.com>
2021-02-13 21:00:17 +0800

    Update env.bat

--- env.bat
+++ env.bat
@@ -8,7 +8,7 @@ set BOOST_ROOT=C:\oime\boost
 rem OPTIONAL: Visual Studio version and platform toolset
 rem set BJAM_TOOLSET=msvc-14.1
 rem set CMAKE_GENERATOR="Visual Studio 15 2017"
-rem set PLATFORM_TOOLSET=v141_xp
+rem set PLATFORM_TOOLSET=v141
 
 rem OPTIONAL: path to additional build tools
 rem set DEVTOOLS_PATH=%ProgramFiles%\Git\cmd;%ProgramFiles%\CMake\bin;C:\Python27;
